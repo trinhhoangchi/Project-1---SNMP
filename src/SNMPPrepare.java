4d954d2a10a6ace1e8b99bad72aaae464f71dc77
huynqkk <gr2922004@gmail.com>
2024-12-28 17:07:23 +0700

    Update SNMPPrepare.java

--- src/SNMPPrepare.java
+++ src/SNMPPrepare.java
@@ -3,40 +3,30 @@ import org.snmp4j.smi.GenericAddress;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.mp.SnmpConstants;
 
-
-
   class SnmpResult {
         private final String oid;
         private final String name;
-        private final String type;
         private final String value;
 
-        public SnmpResult(String oid, String name, String type, String value) {
+        public SnmpResult(String oid, String name, String value) {
             this.oid = oid;
             this.name = name;
-            this.type = type;
             this.value = value;
         }
 
         public String getOid() {
             return oid;
         }
-
         public String getName() {
             return name;
         }
-
-        public String getType() {
-            return type;
-        }
-
         public String getValue() {
             return value;
         }
 
         @Override
         public String toString() {
-            return String.format("OID: %s, Name: %s, Type: %s, Value: %s", oid, name, type, value);
+            return String.format("OID: %s, Name: %s, Type: %s, Value: %s", oid, name, value);
         }
     }
     public class SNMPPrepare {
