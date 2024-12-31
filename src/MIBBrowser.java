3240e23e283b9000165a8d63c990def354fdb346
huynqkk <gr2922004@gmail.com>
2024-12-31 15:04:58 +0700

    Update MIBBrowser.java (Thêm trường nhập ip và trường nhập community String)

--- src/MIBBrowser.java
+++ src/MIBBrowser.java
@@ -8,42 +8,60 @@ public class MIBBrowser extends JFrame {
     private JTree mibTree;
     private JTable infoTable, resultTable;
     private JTextField oidField;
+    private JTextField ipField;
+    private JTextField csField;
     private JLabel oidDisplayLabel;
+
 
     public MIBBrowser() {
         setTitle("MIB-2 Browser");
         setSize(1000, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-
+        setLocation(140 ,40);
+     //   setLayout(new BorderLayout());
+//-----------------
         JPanel oidPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); 
-        oidPanel.setPreferredSize(new Dimension(1000, 60));
-
-        JLabel oidLabel = new JLabel("Enter OID: ");
+        oidPanel.setPreferredSize(new Dimension(1000, 50));
+        JLabel oidLabel = new JLabel("  OID: ");
         oidField = new JTextField(15);
+
+        JLabel ipLabel = new JLabel("IP: ");
+        ipField = new JTextField(15);
+
+        JLabel csLabel = new JLabel("Community String: ");
+        csField = new JTextField(20);
+
         JButton walkButton = new JButton("Walk");
         JButton getButton = new JButton("Get");
         oidDisplayLabel = new JLabel("OID: Not connected");
 
+
         oidPanel.add(oidLabel);
         oidPanel.add(oidField);
+        oidPanel.add(ipLabel);
+        oidPanel.add(ipField);
+        oidPanel.add(csLabel);
+        oidPanel.add(csField);
         oidPanel.add(walkButton);
         oidPanel.add(getButton);
         oidPanel.add(oidDisplayLabel);
-
+//--------------- Tạo cây MIB
         DefaultMutableTreeNode root = MIBTree.createMIBTree();
         mibTree = new JTree(root);
-        mibTree.addTreeSelectionListener(e -> MIBInfoDisplay.displayNodeInfo(e.getPath(), infoTable, oidField));
+
+        //Lắng nghe người dùng chọn 1 nút trên cây 
+        mibTree.addTreeSelectionListener(event -> MIBInfoDisplay.displayNodeInfo(event.getPath(), infoTable, oidField));
         JScrollPane treeScrollPane = new JScrollPane(mibTree);
         treeScrollPane.setPreferredSize(new Dimension(250, 300));
 
-        String[] infoColumns = {"Attribute", "Value"};
+//---------------- Tạo bảng infoTable hiển thị thông tin của nút
+    String[] infoColumns = {"Attribute", "Value"};
         infoTable = new JTable(new DefaultTableModel(infoColumns, 0));
         JScrollPane infoScroll = new JScrollPane(infoTable);
 
         JSplitPane leftSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, treeScrollPane, infoScroll); 
         leftSplit.setDividerLocation(300);
 
+//----------------- Tạo bảng hiển thị kết quả         
         String[] resultColumns = {"OID", "Name", "Value"};
         resultTable = new JTable(new DefaultTableModel(resultColumns, 0));
         JScrollPane resultScroll = new JScrollPane(resultTable);
@@ -62,15 +80,28 @@ public class MIBBrowser extends JFrame {
 
     private void onWalkButtonPressed() {
         String rootOid = oidField.getText();
+        String ip = ipField.getText();
+        String community = csField.getText();
         if (rootOid.isEmpty()) {
-            oidDisplayLabel.setText("OID: Please enter a valid OID.");
+            JOptionPane.showMessageDialog(this,"Please enter a valid OID.",
+                                        "Input Error",JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        if (ip.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid IP.",
+                                         "Input Error", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        if (community.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid Community String.", 
+                                            "Input Error", JOptionPane.WARNING_MESSAGE);
             return;
         }
         oidDisplayLabel.setText("Root OID: " + rootOid);
 
         try {
             Walk walk = new Walk();
-            List<SnmpResult> result = walk.doWalk(rootOid, "localhost"); 
+            List<SnmpResult> result = walk.doWalk(rootOid, ip, community); 
             DefaultTableModel tableModel = (DefaultTableModel) resultTable.getModel();
             tableModel.setRowCount(0);
 
@@ -79,21 +110,34 @@ public class MIBBrowser extends JFrame {
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error performing SNMP walk: " + e.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+                                            "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     private void onGetButtonPressed() {
         String oid = oidField.getText();
+        String ip = ipField.getText();
+        String community = csField.getText();
         if (oid.isEmpty()) {
-            oidDisplayLabel.setText("OID: Please enter a valid OID.");
+            JOptionPane.showMessageDialog(this,"Please enter a valid OID.",
+                                            "Input Error",JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        if (ip.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid IP.", 
+                                            "Input Error", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        if (community.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid Community String.", 
+                                            "Input Error", JOptionPane.WARNING_MESSAGE);
             return;
         }
         oidDisplayLabel.setText("OID: " + oid);
 
         try {
-            Get get = new Get(); // Create an instance of Get
-            List<SnmpResult> result = get.doGet(List.of(oid), "localhost");
+            Get get = new Get(); 
+            List<SnmpResult> result = get.doGet(List.of(oid), ip,community);
             DefaultTableModel tableModel = (DefaultTableModel) resultTable.getModel();
             tableModel.setRowCount(0);
 
@@ -102,7 +146,7 @@ public class MIBBrowser extends JFrame {
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error performing SNMP get: " + e.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+                                            "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
