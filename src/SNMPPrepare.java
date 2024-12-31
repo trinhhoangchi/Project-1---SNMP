b6eee85319eecb841366fcdca5328f3e055c8f39
huynqkk <gr2922004@gmail.com>
2024-12-31 15:03:37 +0700

    Update SNMPPrepare.java

--- src/SNMPPrepare.java
+++ src/SNMPPrepare.java
@@ -3,33 +3,7 @@ import org.snmp4j.smi.GenericAddress;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.mp.SnmpConstants;
 
-  class SnmpResult {
-        private final String oid;
-        private final String name;
-        private final String value;
-
-        public SnmpResult(String oid, String name, String value) {
-            this.oid = oid;
-            this.name = name;
-            this.value = value;
-        }
-
-        public String getOid() {
-            return oid;
-        }
-        public String getName() {
-            return name;
-        }
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("OID: %s, Name: %s, Type: %s, Value: %s", oid, name, value);
-        }
-    }
-    public class SNMPPrepare {
+public class SNMPPrepare {
     protected CommunityTarget createTarget(String ip, String community) {
         CommunityTarget target = new CommunityTarget();
         target.setAddress(GenericAddress.parse("udp:" + ip + "/161"));
